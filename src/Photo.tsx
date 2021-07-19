49fa0501beb8e7aa934b74843f81f1d36e5088cc
Saurabh Anand <43960242+saurabhanand3390@users.noreply.github.com>
2021-07-19 18:39:35 +0530

    Added lazy loading

--- src/Photo.tsx
+++ src/Photo.tsx
@@ -1,4 +1,5 @@
 import React, {forwardRef} from 'react';
+import LazyLoad from "react-lazyload";
 
 export const Photo = forwardRef(({url, index, faded, style, ...props}: any, ref: React.LegacyRef<HTMLDivElement> | undefined) => {
 		const inlineStyles = {
@@ -7,12 +8,24 @@ export const Photo = forwardRef(({url, index, faded, style, ...props}: any, ref:
 				height: index === 0 ? 410 : 200,
 				gridRowStart: index === 0 ? 'span 2' : null,
 				gridColumnStart: index === 0 ? 'span 2' : null,
-				backgroundImage: `url("${url}")`,
+				// backgroundImage: `url("${url}")`,
 				backgroundSize: 'cover',
 				backgroundPosition: 'center',
-				backgroundColor: 'grey',
+			backgroundColor: 'grey',
+				overflow: 'hidden',
 				...style,
 		};
 		
-		return <div ref={ref} style={inlineStyles} {...props} />;
+		return (
+      
+			<div ref={ref} style={inlineStyles} {...props}>
+				<LazyLoad height={200}>
+          <img
+            src={url}
+            style={{ width: "100%", maxWidth: "100%" }}
+            alt="newIma"
+          />
+      </LazyLoad>
+        </div>
+    );
 });
