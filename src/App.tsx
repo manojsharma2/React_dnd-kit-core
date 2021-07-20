6c278a738914590841b793026d87c19651856c89
Manoj Sharma2 <manoj.sharma2@3pillar.corp>
2021-07-20 11:22:13 +0530

    multi_drag in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,17 @@
-import React, {useState} from 'react';
-import {
-  DndContext,
-  closestCenter,
-  MouseSensor,
-  TouchSensor,
-  DragOverlay,
-  useSensor,
-  useSensors,
-} from '@dnd-kit/core';
-import {
-  arrayMove,
-  SortableContext,
-  rectSortingStrategy,
-} from '@dnd-kit/sortable';
+import React from "react";
+import {rectSortingStrategy} from "@dnd-kit/sortable";
 
-import {Grid} from './Grid';
-import {SortablePhoto} from './SortablePhoto';
-import {Photo} from './Photo';
-import photos from './photos.json';
+import {Selectable} from "./SelectTable/Selectable";
 
-const UploadGallery = () => {
-  const [items, setItems] = useState(photos);
-  const [activeId, setActiveId] = useState('');
-  const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor));
-  
+export default function App() {
   return (
-     <DndContext
-        sensors={sensors}
-        collisionDetection={closestCenter}
-        onDragStart={handleDragStart}
-        onDragEnd={handleDragEnd}
-        onDragCancel={handleDragCancel}
-     >
-       <SortableContext items={items} strategy={rectSortingStrategy}>
-         <Grid columns={4}>
-           {items.map((url, index) => (
-              <SortablePhoto key={index} url={url} index={index} />
-           ))}
-         </Grid>
-       </SortableContext>
-       
-       <DragOverlay adjustScale={true}>
-         {activeId ? (
-            <Photo url={activeId} index={items.indexOf(activeId)} />
-         ) : null}
-       </DragOverlay>
-     </DndContext>
+     <Selectable
+        columns={3}
+        strategy={rectSortingStrategy}
+        wrapperStyle={() => ({
+          width: 150,
+          height: 150
+        })}
+     />
   );
-  
-  function handleDragStart(event: any) {
-    setActiveId(event.active.id);
-  }
-  
-  function handleDragEnd(event: any) {
-    const {active, over} = event;
-    
-    if (active.id !== over.id) {
-      setItems(() => {
-        const oldIndex = items.indexOf(active.id);
-        const newIndex = items.indexOf(over.id);
-        
-        return arrayMove(items, oldIndex, newIndex);
-      });
-    }
-    
-    setActiveId('');
-  }
-  
-  function handleDragCancel() {
-    setActiveId('');
-  }
-};
-
-export default UploadGallery;
+}
---
+++ src/SelectTable/List.tsx
@@ -0,0 +1,32 @@
+import React, { forwardRef } from "react";
+import cx from "classnames";
+
+import styles from "./List.module.scss";
+
+export interface Props {
+		children: React.ReactNode;
+		columns?: number;
+		style?: React.CSSProperties;
+		horizontal?: boolean;
+}
+
+export const List = forwardRef<HTMLUListElement, Props>(
+			({ children, columns = 1, horizontal, style }: Props, ref) => {
+					return (
+								<ul
+											ref={ref}
+											style={
+													{
+															...style,
+															"--columns": columns
+													} as React.CSSProperties
+											}
+											className={cx({
+													[styles.List]: true
+											})}
+								>
+										{children}
+								</ul>
+					);
+			}
+);
